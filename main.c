2bc0f88d5f76e8387ed01c6ea735afe52c2c768c
Elias <eliasander@proton.me>
2026-03-12 13:40:46 +0100

    Basic button rendering with primay and secondary color variables.

--- main.c
+++ main.c
@@ -10,6 +10,9 @@ int main(void)
     int screenWidth = 1024;
     int screenHeight = 600;
 
+    Color primaryColor = (Color){ 255, 161, 0, 255 };
+    Color secondaryColor = (Color){200,200,200,255};
+
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -26,17 +29,18 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLANK);
 
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         Rectangle start = {
-            (float)screenWidth / 2.0f - 50.0f / 2.0f,
-            (float)screenHeight - 75.0f,
-            50,
-            50
+            (float)screenWidth / 2.0f - 200.0f / 2.0f,
+            (float)screenHeight - 225.0f,
+            200,
+            100
         };
-        DrawRectangleRounded(start, 0.2f, 2, GREEN);
+        DrawRectangleRounded(start, 0.2f, 2, secondaryColor);
+        DrawRectangleRoundedLinesEx(start, 0.2f, 2, 6.0f, primaryColor);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
